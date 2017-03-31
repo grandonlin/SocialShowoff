6fe716fc0c63bf8eb421ae304fb73fe4768eda10
Grandon Lin <glin@Grandons-MacBook-Pro.local>
2017-03-30 21:51:53 -0400

    Completed setting up Firebase integration

---
+++ SocialShowoff/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  SignInVC.swift
+//  SocialShowoff
+//
+//  Created by Grandon Lin on 2017-03-29.
+//  Copyright © 2017 Grandon Lin. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
